6472e53415d659806f14fa3555e3af394bcca394
Guillaume D. <guil.duchesne@gmail.com>
2025-12-16 14:20:30 +0100

    test

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "A LA HEH, Monsieur TRICARICO trouve le cours"
+    return "A LA HEH, Monsieur TRICARICO trouve le cours test"
 
 @app.route("/ping")
 def ping():
